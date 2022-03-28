0036d8f6cbfc0185cdba4a428fd4ae4d156ee870
SiWon Lee <tldnjs0821222@naver.com>
2022-03-28 18:12:50 +0900

    10828

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,47 +1,77 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-struct cnf
-{
-	int start;
-	int end;
-};
+int stack[100]={0};
+int topindex=-1;
 
-bool cmp(cnf t1, cnf t2)
+void push(int X)
 {
-	if(t1.end == t2.end)
-		return t1.start < t2.start;
-	else
-		return t1.end < t2.end;
+    topindex++;
+    stack[topindex]=X;
+}
+
+void pop()
+{
+    if(topindex==-1)
+    {
+        cout << "-1\n";
+        return ;
+    }
+    cout << stack[topindex] << "\n";
+    stack[topindex]=0;
+    topindex--;
+}
+
+void size()
+{
+    cout << topindex+1 << "\n";
+}
+
+void empty()
+{
+    if(topindex == -1)
+        cout << "1\n";
+    else
+        cout << "0\n";
+}
+
+void top()
+{
+    if(topindex == -1)
+    {
+        cout << "-1\n";
+        return ;
+    }
+    cout << stack[topindex] << "\n";
 }
 
 int main()
 {
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	int n=0;
-	cin >> n;
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+    string cmd="";
+    int opr;
+    for(int i=0; i<n; i++)
+    {
+        cin >> cmd;
+        if(cmd == "push")
+        {
+            cin >> opr;
+            push(opr);
+        }
+        else if(cmd == "pop")
+            pop();
+        else if(cmd == "size")
+            size();
+        else if(cmd == "empty")
+            empty();
+        else if(cmd == "top")
+            top();
+    }
 
-	vector<cnf> v(n);
-	for(int i=0; i<n; i++)
-		cin >> v[i].start >> v[i].end;
-
-	sort(v.begin(), v.end(), cmp);
-	
-	int cnt=0;
-	int a=0;
-	for(int i=0; i<v.size(); i++)
-	{
-		if(a <= v[i].start)
-		{
-			a = v[i].end;
-			cnt++;
-		}
-	}
-
-	cout << cnt;
 }
