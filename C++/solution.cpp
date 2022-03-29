f23df4f2523839e6476831049fc5d433bcf6b488
SiWon Lee <tldnjs0821222@naver.com>
2022-03-29 20:02:21 +0900

    10799

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -3,49 +3,29 @@
 
 using namespace std;
 
-void stackprint(stack<char> &stack)
-{
-    while(!stack.empty())
-    {
-        cout << stack.top();
-        stack.pop();
-    }
-}
+
 
 int main()
 {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    
     string s;
-    getline(cin, s);
-    stack<char>stk;
-
-    for(int i=0; i<s.length(); i++)
+    cin >> s;
+    int open=0;
+    int sum=0;
+    for(int i=0; i<s.size()-1; i++)
     {
-        if(s[i]=='<')
+        if(s[i]=='(' && s[i+1]=='(')
         {
-            stackprint(stk);
-            while(1)
-            {
-                if(s[i]=='>')
-                {
-                    cout << '>';
-                    break;
-                }
-                cout << s[i];
-                i++;
-            }
+            open++;
         }
-        else if(s[i]==' ')
+        else if(s[i]=='(' && s[i+1]==')')
         {
-            stackprint(stk);
-            cout << ' ';
+            sum += open;
         }
-        else
+        else if(s[i]==')' && s[i+1]==')')
         {
-            stk.push(s[i]);
+            sum += 1;
+            open--;
         }
     }
-    stackprint(stk);
+    cout << sum;
 }
