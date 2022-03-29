2d39982c56ec4c41e48ad2e9d17782a94bdf7bf5
SiWon Lee <tldnjs0821222@naver.com>
2022-03-29 14:33:06 +0900

    1158

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -8,53 +8,31 @@ int main()
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
     
+    int n, k;
+    cin >> n >> k;
     queue<int>q;
-    int n;
-    cin >> n;
+    int que[n];
+    for(int i=1; i<n+1; i++)
+        q.push(i);
     for(int i=0; i<n; i++)
     {
-        string cmd;
-        cin >> cmd;
-        if(cmd == "push")
+        int temp;
+        for(int j=0; j<k-1; j++)
         {
-            int opr;
-            cin >> opr;
-            q.push(opr);
+            temp=q.front();
+            q.pop();
+            q.push(temp);
         }
-        if(cmd == "pop")
-        {
-            if(!q.empty())
-            {
-                cout << q.front() << "\n";
-                q.pop();
-            }
-            else
-                cout << "-1\n";
-        }
-        if(cmd == "size")
-        {
-            cout << q.size() << "\n";
-        }
-        if(cmd == "empty")
-        {
-            if(!q.empty())
-                cout << "0\n";
-            else
-                cout << "1\n";
-        }
-        if(cmd == "front")
-        {
-            if(!q.empty())
-                cout << q.front() << "\n";
-            else
-                cout << "-1\n";
-        }
-        if(cmd == "back")
-        {
-            if(!q.empty())
-                cout << q.back() << "\n";
-            else
-                cout << "-1\n";
-        }
+        que[i]=q.front();
+        q.pop();
     }
+    cout << "<";
+    for(int i=0; i<n; i++)
+    {
+        if(i==0)
+            cout << que[i];
+        else
+            cout << ", " << que[i];
+    }
+    cout << ">";
 }
