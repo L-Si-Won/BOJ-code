e2b3c90e1896c629d09eff809a828a222f6c5f2f
SiWon Lee <tldnjs0821222@naver.com>
2022-03-31 20:00:53 +0900

    10808

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <stack>
 #include <vector>
 
 using namespace std;
@@ -8,52 +7,11 @@ int main()
 {
     string s;
     cin >> s;
-    stack<char>stk;
-    vector<char>v;
-
+    vector<int>a(26, 0);
     for(int i=0; i<s.size(); i++)
     {
-        if(s[i] >= 'A' && s[i] <= 'Z')
-        {
-            v.push_back(s[i]);
-        }
-        else if(s[i]=='(')
-        {
-            stk.push(s[i]);
-        }
-        else if(s[i]==')')
-        {
-            while(!stk.empty() && stk.top()!='(')
-            {
-                v.push_back(stk.top());
-                stk.pop();
-            }
-            stk.pop();
-        }
-        else if(s[i]=='+' || s[i]=='-')
-        {
-            while(!stk.empty() && stk.top()!='(')
-            {
-                v.push_back(stk.top());
-                stk.pop();
-            }
-            stk.push(s[i]);
-        }
-        else if(s[i]=='*' || s[i]=='/')
-        {
-            while(!stk.empty() && (stk.top()=='/' || stk.top()=='*'))
-            {
-                v.push_back(stk.top());
-                stk.pop();
-            }
-            stk.push(s[i]);
-        }
+        a[s[i]-97]++;
     }
-    while(!stk.empty())
-    {
-        v.push_back(stk.top());
-        stk.pop();
-    }
-    for(int i=0; i<v.size(); i++)
-        cout << v[i]; 
+    for(int i=0; i<26; i++)
+        cout << a[i] << " ";
 }
