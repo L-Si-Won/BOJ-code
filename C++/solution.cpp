7b083c9931bd76e71470159490161bbd0120f41a
SiWon Lee <tldnjs0821222@naver.com>
2022-03-31 21:54:43 +0900

    10824

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -5,30 +5,10 @@ using namespace std;
 
 int main()
 {
-    string s;
-    getline(cin, s);
-    char temp[s.size()];
-    for(int i=0; i<s.size(); i++)
-    {
-        if(s[i]>='a' && s[i]<='m')
-        {
-            temp[i]=s[i]+13;
-        }
-        else if(s[i]>='n' && s[i]<='z')
-        {
-            temp[i]='a'+s[i]-110;
-        }
-        else if(s[i]>='A' && s[i]<='M')
-        {
-            temp[i]=s[i]+13;
-        }
-        else if(s[i]>='N' && s[i]<='Z')
-        {
-            temp[i]='A'+s[i]-78;
-        }
-        else
-            temp[i]=s[i];
-    }
-    for(int i=0; i<s.size(); i++)
-        cout << temp[i];
+    string a,b,c,d;
+    cin >> a >> b >> c >> d;
+    long long A, B;
+    A=stoll(a+b);
+    B=stoll(c+d);
+    cout << A+B;
 }
