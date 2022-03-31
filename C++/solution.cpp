63e2b1a03e7efd1b4ff4edb1ef8048ec1257aaae
SiWon Lee <tldnjs0821222@naver.com>
2022-03-31 19:54:09 +0900

    1918

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -6,65 +6,54 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
     string s;
     cin >> s;
-    vector<pair<char, float>>v(n);
-    stack<double>stk;
-    double temp=0;
-    char save='A';
-    for(int i=0; i<n; i++)
-    {
-        v[i].first=save++;
-        cin >> v[i].second;
-    }
+    stack<char>stk;
+    vector<char>v;
+
     for(int i=0; i<s.size(); i++)
     {
         if(s[i] >= 'A' && s[i] <= 'Z')
         {
-            for(int j=0; j<26; j++)
+            v.push_back(s[i]);
+        }
+        else if(s[i]=='(')
+        {
+            stk.push(s[i]);
+        }
+        else if(s[i]==')')
+        {
+            while(!stk.empty() && stk.top()!='(')
             {
-                if(v[j].first==s[i])
-                {
-                    stk.push(v[j].second);
-                }
+                v.push_back(stk.top());
+                stk.pop();
             }
+            stk.pop();
         }
-        if(s[i] == '*')
+        else if(s[i]=='+' || s[i]=='-')
         {
-            temp=stk.top();
-            stk.pop();
-            temp = stk.top() * temp;
-            stk.pop();
-            stk.push(temp);
+            while(!stk.empty() && stk.top()!='(')
+            {
+                v.push_back(stk.top());
+                stk.pop();
+            }
+            stk.push(s[i]);
         }
-        if(s[i] == '/')
+        else if(s[i]=='*' || s[i]=='/')
         {
-            temp=stk.top();
-            stk.pop();
-            temp = stk.top() / temp;
-            stk.pop();
-            stk.push(temp);
-        }
-        if(s[i] == '+')
-        {
-            temp=stk.top();
-            stk.pop();
-            temp = stk.top() + temp;
-            stk.pop();
-            stk.push(temp);
-        }
-        if(s[i] == '-')
-        {
-            temp=stk.top();
-            stk.pop();
-            temp = stk.top() - temp;
-            stk.pop();
-            stk.push(temp);
+            while(!stk.empty() && (stk.top()=='/' || stk.top()=='*'))
+            {
+                v.push_back(stk.top());
+                stk.pop();
+            }
+            stk.push(s[i]);
         }
     }
-    cout << fixed;
-    cout.precision(2);
-    cout << stk.top();
+    while(!stk.empty())
+    {
+        v.push_back(stk.top());
+        stk.pop();
+    }
+    for(int i=0; i<v.size(); i++)
+        cout << v[i]; 
 }
