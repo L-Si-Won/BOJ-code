93f8c9427c16fb55a54ef8afedee336af49b31f5
SiWon Lee <tldnjs0821222@naver.com>
2022-04-02 21:13:07 +0900

    2217

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    string a,b,c,d;
-    cin >> a >> b >> c >> d;
-    long long A, B;
-    A=stoll(a+b);
-    B=stoll(c+d);
-    cout << A+B;
+    int n;
+    cin >> n;
+    vector<int>v;
+    int a;
+    for(int i=0; i<n; i++)
+    {
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(), v.end());
+    vector<int>temp;
+    for(int i=0; i<n; i++)
+    {
+        temp.push_back((n-i) * v[i]);
+    }
+    int max=temp[0];
+    for(int i=1; i<n; i++)
+    {
+        if(max < temp[i])
+            max = temp[i];
+    }
+    cout << max;
 }
