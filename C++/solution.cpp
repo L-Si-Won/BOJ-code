daa43e69369d4fccb123ced74d2196fff6a5d8e2
SiWon Lee <tldnjs0821222@naver.com>
2022-04-03 19:55:49 +0900

    11727

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -7,9 +7,9 @@ int d[1001];
 int dp(int i)
 {
     if(i==1) return 1;
-    if(i==2) return 2;
+    if(i==2) return 3;
     if(d[i]!=0) return d[i];
-    return d[i]=(dp(i-1)+dp(i-2))%10007;
+    return d[i]=(dp(i-1)+dp(i-2)*2)%10007;
 }
 
 int main()
