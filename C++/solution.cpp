d37d27720797e8b04de974aa50745d116ab83102
SiWon Lee <tldnjs0821222@naver.com>
2022-04-03 19:52:06 +0900

    11726

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -2,37 +2,14 @@
 
 using namespace std;
 
-int a[1000001];
+int d[1001];
 
 int dp(int i)
 {
-    if(i==1)    return 0;
-    if(i==2)    return 1;
-    if(i==3)    return 1;
-
-    if(i%2==0 && i%3==0)
-    {
-        if(a[i/3] > a[i/2])
-            return a[i/2]+1;
-        else
-            return a[i/3]+1;
-    }
-    else if(i%3==0 && i%2!=0)
-    {
-        if( a[i/3] > a[i-1] )
-            return a[i-1]+1;
-        else
-            return a[i/3]+1;
-    }
-    else if(i%2==0 && i%3!=0)
-    {
-        if( a[i/2] > a[i-1] )
-            return a[i-1]+1;
-        else
-            return a[i/2]+1;
-    }
-    else
-        return a[i-1]+1;
+    if(i==1) return 1;
+    if(i==2) return 2;
+    if(d[i]!=0) return d[i];
+    return d[i]=(dp(i-1)+dp(i-2))%10007;
 }
 
 int main()
@@ -40,9 +17,5 @@ int main()
     int n;
     cin >> n;
 
-    for(int i=1; i<=n; i++)
-    {
-        a[i]=dp(i);
-    }
-    cout << a[n];
+    cout << dp(n);
 }
