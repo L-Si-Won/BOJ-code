3461ebd4ee18af2c687ff703910b986ffd7e641e
SiWon Lee <tldnjs0821222@naver.com>
2022-04-04 16:17:46 +0900

    9095

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int d[1001];
+vector<int>v(11,0);
 
-int dp(int i)
+int dp(vector<int> &v, int i)
 {
     if(i==1) return 1;
-    if(i==2) return 3;
-    if(d[i]!=0) return d[i];
-    return d[i]=(dp(i-1)+dp(i-2)*2)%10007;
+    if(i==2) return 2;
+    if(i==3) return 4;
+    if(v[i]!=0) return v[i];
+    v.push_back(dp(v,i-1)+dp(v,i-2)+dp(v,i-3));
+    return v.back();
 }
 
 int main()
 {
+    int test;
+    cin >> test;
     int n;
-    cin >> n;
-
-    cout << dp(n);
+    for(int i=0; i<test; i++)
+    {
+        cin >> n;
+        cout << dp(v, n) << "\n";
+    }
 }
