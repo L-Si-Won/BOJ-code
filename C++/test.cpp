d2a9ef818f7244d1ef8774451df1dc08ee7c8340
SiWon Lee <tldnjs0821222@naver.com>
2022-04-05 22:47:06 +0900

    14002

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,49 +1,37 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int stack[100]={0};
-int topindex=-1;
-
-void push(int X)
+int d[1001];
+int a[1001];
+vector<int> temp[1001];
+vector<int> sol;
+void dp(int n)
 {
-    topindex++;
-    stack[topindex]=X;
-}
-
-void pop()
-{
-    if(topindex==-1)
+    for(int i=1; i<n+1; i++)
     {
-        cout << "-1\n";
-        return ;
+        d[i]=1;
+        temp[i].push_back(a[i]);
+        for(int j=i-1; j>0; j--)
+        {
+            if(a[i] > a[j])
+            {
+                if(d[i] < d[j]+1)
+                {
+                    d[i]=d[j]+1;
+                    temp[i].clear(); //this command can erase
+                    temp[i]=temp[j];
+                    temp[i].push_back(a[i]);
+                }
+            }
+        }
+        if(sol.size() < temp[i].size())
+            sol=temp[i];
     }
-    cout << stack[topindex] << "\n";
-    stack[topindex]=0;
-    topindex--;
-}
-
-void size()
-{
-    cout << topindex+1 << "\n";
-}
-
-void empty()
-{
-    if(topindex == -1)
-        cout << "1\n";
-    else
-        cout << "0\n";
-}
-
-void top()
-{
-    if(topindex == -1)
-    {
-        cout << "-1\n";
-        return ;
-    }
-    cout << stack[topindex] << "\n";
+    cout << sol.size() << "\n";
+    for(int i=0; i<sol.size(); i++)
+        cout << sol[i] << " ";
 }
 
 int main()
@@ -51,27 +39,10 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    
+
     int n;
     cin >> n;
-    string cmd="";
-    int opr;
-    for(int i=0; i<n; i++)
-    {
-        cin >> cmd;
-        if(cmd == "push")
-        {
-            cin >> opr;
-            push(opr);
-        }
-        else if(cmd == "pop")
-            pop();
-        else if(cmd == "size")
-            size();
-        else if(cmd == "empty")
-            empty();
-        else if(cmd == "top")
-            top();
-    }
-
+    for(int i=1; i<n+1; i++)
+        cin >> a[i];
+    dp(n);
 }
