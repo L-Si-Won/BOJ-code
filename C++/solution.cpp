d86deda688d4f38f71a5f5a746afdf50c22abc97
SiWon Lee <tldnjs0821222@naver.com>
2022-04-06 22:07:30 +0900

    13398

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int d[100001];
+int d[100001][2];
 int a[100001];
 void dp(int n)
 {
-    d[1]=a[1];
+    d[1][0]=a[1];
+    d[1][1]=a[1];
+    int sol=a[1];
     for(int i=2; i<n+1; i++)
     {
-        d[i]=a[i];
-        if(d[i] < d[i-1]+a[i])
-            d[i]=d[i-1]+a[i];
+        d[i][0]=a[i];
+        if(d[i][0] < d[i-1][0]+a[i])
+            d[i][0]=d[i-1][0]+a[i];
+        if(d[i-1][0] < a[i]+d[i-1][1])
+            d[i][1]=a[i]+d[i-1][1];
+        else
+            d[i][1]=d[i-1][0];
     }
 }
 
@@ -26,9 +33,13 @@ int main()
     for(int i=1; i<n+1; i++)
         cin >> a[i];
     dp(n);
-    int max=d[1];
+    int max0=d[1][0];
     for(int i=2; i<n+1; i++)
-        if(max < d[i])
-            max=d[i];
-    cout << max;
+        if(max0 < d[i][0])
+            max0=d[i][0];
+    int max1=d[1][1];
+    for(int i=2; i<n+1; i++)
+        if(max1 < d[i][1])
+            max1=d[i][1];
+    cout << max(max0, max1);
 }
