329737846d59a6860e77f775e8e59e007a2e48df
SiWon Lee <tldnjs0821222@naver.com>
2022-04-07 21:31:40 +0900

    1699

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
-
-int d[100001][2];
-int a[100001];
+#define max 100001
+int d[max];
 void dp(int n)
 {
-    d[1][0]=a[1];
-    d[1][1]=a[1];
-    int sol=a[1];
-    for(int i=2; i<n+1; i++)
+    d[1]=1;
+    d[2]=2;
+    d[3]=3;
+    int min=0;
+
+    for(int i=4; i<n+1; i++)
     {
-        d[i][0]=a[i];
-        if(d[i][0] < d[i-1][0]+a[i])
-            d[i][0]=d[i-1][0]+a[i];
-        if(d[i-1][0] < a[i]+d[i-1][1])
-            d[i][1]=a[i]+d[i-1][1];
-        else
-            d[i][1]=d[i-1][0];
+        d[i]=d[i-1]+1;
+        for(int j=1; j*j<=i; j++)
+        {
+            if(d[i-j*j]+1 < d[i])
+                d[i]=d[i-j*j]+1;
+        }
     }
 }
 
@@ -30,16 +29,6 @@ int main()
 
     int n;
     cin >> n;
-    for(int i=1; i<n+1; i++)
-        cin >> a[i];
     dp(n);
-    int max0=d[1][0];
-    for(int i=2; i<n+1; i++)
-        if(max0 < d[i][0])
-            max0=d[i][0];
-    int max1=d[1][1];
-    for(int i=2; i<n+1; i++)
-        if(max1 < d[i][1])
-            max1=d[i][1];
-    cout << max(max0, max1);
+    cout << d[n];
 }
