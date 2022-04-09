c7824bfdee9b302329b9dbff5866890e35b1faa9
SiWon Lee <tldnjs0821222@naver.com>
2022-04-09 20:33:20 +0900

    1309

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -3,34 +3,24 @@
 
 using namespace std;
 
-int d[1001][3];
-int c[1001][3];
-int dd[9];
-
+int mod=9901;
+int d[100001][3];
 int dp(int n)
 {
-    int mini = 987654321;
-    for (int j = 0; j < 3; j++)
+    int sum=0;
+    d[1][0]=1;
+    d[1][1]=1;
+    d[1][2]=1;
+    for(int i=2; i<n+1; i++)
     {
-        for (int i = 0; i < 3; i++)
-        {
-            if (j == i)
-                d[1][i] = c[1][i];
-            else
-                d[1][i] = 987654321;
-        }
-        for (int i = 2; i < n + 1; i++)
-        {
-            d[i][0] = min(d[i - 1][1], d[i - 1][2]) + c[i][0];
-            d[i][1] = min(d[i - 1][2], d[i - 1][0]) + c[i][1];
-            d[i][2] = min(d[i - 1][0], d[i - 1][1]) + c[i][2];
-        }
-        for (int i = 0; i < 3; i++)
-            if (j != i)
-                mini = min(mini, d[n][i]);
+        d[i][0]=(d[i-1][0]+d[i-1][1]+d[i-1][2])%mod;
+        d[i][1]=(d[i-1][0]+d[i-1][2])%mod;
+        d[i][2]=(d[i-1][0]+d[i-1][1])%mod;
     }
-    return mini;
+    sum = (d[n][0]+d[n][1]+d[n][2])%mod;
+    return sum;
 }
+
 int main()
 {
     cin.tie(NULL);
@@ -39,7 +29,5 @@ int main()
 
     int n;
     cin >> n;
-    for (int i = 1; i < n + 1; i++)
-        cin >> c[i][0] >> c[i][1] >> c[i][2];
     cout << dp(n);
 }
