49adba943ac53a1c37975798675e0e923fa373b6
SiWon Lee <tldnjs0821222@naver.com>
2022-04-10 18:39:45 +0900

    1932

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -3,21 +3,22 @@
 
 using namespace std;
 
-int d[100001][3];
-int a[3][100001];
+int d[10001][3];
+int a[10001];
 int dp(int n)
 {
     int ans;
+    int three=1;
     d[1][0]=0;
-    d[1][1]=a[1][1];
-    d[1][2]=a[2][1];
+    d[1][1]=a[1];
+    d[1][2]=a[1];
     for(int i=2; i<n+1; i++)
     {
         d[i][0]=max(max(d[i-1][0], d[i-1][1]), d[i-1][2]);
-        d[i][1]=max(d[i-1][0], d[i-1][2])+a[1][i];
-        d[i][2]=max(d[i-1][0], d[i-1][1])+a[2][i];
+        d[i][1]=d[i-1][0]+a[i];
+        d[i][2]=d[i-1][1]+a[i];
     }
-    ans = max(max(d[n][0], d[n][1]), d[n][2]);
+    ans = max(max(d[n][0], d[n][1]),d[n][2]);
     return ans;
 }
 
@@ -27,16 +28,9 @@ int main()
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    int test_case;
-    cin >> test_case;
     int n;
-    for(int i=0; i<test_case; i++)
-    {
-        cin >> n;
-        for(int j=1; j<n+1; j++)
-            cin >> a[1][j];
-        for(int j=1; j<n+1; j++)
-            cin >> a[2][j];
-        cout << dp(n) << "\n";
-    }
+    cin >> n;
+    for(int i=1; i<n+1; i++)
+        cin >> a[i];
+    cout << dp(n);
 }
