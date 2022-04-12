3a3a6ad7365dd37600d788daa53b44f9841fd5bf
SiWon Lee <tldnjs0821222@naver.com>
2022-04-12 19:46:29 +0900

    2133

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,37 +1,25 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int d[1001][2];
-int a[1001];
+int d[31];
 int dp(int n)
 {
-    d[1][0]=1;
-    d[1][1]=1;
-    int INC;
-    int DEC;
-    int MAX;
-    for(int i=2; i<n+1; i++)
+    d[2] = 3;
+    d[4] = 11;
+    d[6] = 41;
+    for (int i = 8; i < n + 1; i++)
     {
-        INC=1;
-        DEC=1;
-        for(int j=i-1; j>0; j--)
+        if (i % 2 == 0)
         {
-            if(a[i] > a[j])
-                INC=max(INC, d[j][0]+1);
-            else if(a[i] < a[j])
-                DEC=max(max(d[j][0]+1, d[j][1]+1), DEC);
+            d[i] = d[i - 2] * 3;
+            for (int j = i - 4; j >= 0; j--)
+                d[i] += d[j] * 2;
+            d[i]+=2;
         }
-        d[i][0]=INC;
-        d[i][1]=DEC;
     }
-    MAX=max(d[1][0], d[1][1]);
-    for(int i=2; i<n+1; i++)
-        MAX=max(max(MAX, d[i][0]), d[i][1]);
-    return MAX;
+    return d[n];
 }
-
 int main()
 {
     cin.tie(NULL);
@@ -40,7 +28,5 @@ int main()
 
     int n;
     cin >> n;
-    for(int i=1; i<n+1; i++)
-        cin >> a[i];
     cout << dp(n);
 }
