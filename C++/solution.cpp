5dd99105998c3fe15f5dfa2e47bd2ba03c6d537d
SiWon Lee <tldnjs0821222@naver.com>
2022-04-16 22:10:46 +0900

    1748

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,85 +1,36 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int d[505][505];
-int sum;
-int MAX;
-
-void reset()
-{
-    MAX=max(MAX, sum);
-    sum=0;
-}
-void check(int i, int j)
-{
-    for(int a=i; a<=i+3; a++) //세로모양
-        sum += d[a][j];
-    reset();
-
-    for(int a=j; a<=j+3; a++) //가로모양
-        sum += d[i][a];
-    reset();
-
-    for(int a=i; a<=i+1; a++) //네모모양
-        for(int b=j; b<=j+1; b++)
-            sum += d[a][b];
-    reset();
-
-    //ㅗ모양
-    sum += d[i][j] + d[i-1][j-1] + d[i-1][j] + d[i-1][j+1];
-    reset();
-    sum += d[i][j] + d[i+1][j-1] + d[i+1][j] + d[i+1][j+1];
-    reset();
-    sum += d[i][j] + d[i-1][j-1] + d[i][j-1] + d[i+1][j-1];
-    reset();
-    sum += d[i][j] + d[i-1][j+1] + d[i][j+1] + d[i+1][j+1];
-    reset();
-
-    // ㄱ모양
-    sum += d[i][j] + d[i+1][j] + d[i+2][j] + d[i+2][j+1];
-    reset();
-    sum += d[i][j] + d[i][j+1] + d[i][j+2] + d[i+1][j+2];
-    reset();
-    sum += d[i][j] + d[i][j+1] + d[i+1][j+1] + d[i+2][j+1];
-    reset();
-    sum += d[i][j] + d[i+1][j] + d[i+1][j+1] + d[i+1][j+2];
-    reset();
-    sum += d[i][j] + d[i+1][j] + d[i+2][j] + d[i+2][j-1];
-    reset();
-    sum += d[i][j] + d[i][j+1] + d[i][j+2] + d[i-1][j+2];
-    reset();
-    sum += d[i][j] + d[i-1][j] + d[i-1][j+1] + d[i-1][j+2];
-    reset();
-    sum += d[i][j] + d[i][j-1] + d[i+1][j-1] + d[i+2][j-1];
-    reset();
-
-    //번개모양
-    sum += d[i][j] + d[i+1][j] + d[i+1][j+1] + d[i+2][j+1];
-    reset();
-    sum += d[i][j] + d[i+1][j] + d[i+1][j-1] + d[i+2][j-1];
-    reset();
-    sum += d[i][j] + d[i][j+1] + d[i+1][j+1] + d[i+1][j+2];
-    reset();
-    sum += d[i][j] + d[i][j+1] + d[i-1][j+1] + d[i-1][j+2];
-    reset();
-}
+int cnt;
+int a[10]={0,9,90,900,9000,90000,900000,9000000,90000000,900000000};
 
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    int n, m;
-    cin >> n >> m;
-    for(int i=1; i<n+1; i++)
-        for(int j=1; j<m+1; j++)
-            cin >> d[i][j];
-    for(int i=1; i<n+1; i++){
-        for(int j=1; j<m+1; j++){
-            check(i, j);
+    int n;
+    cin >> n;
+    int idx=1;
+    int temp=n;
+    if(n/10==0){
+        cout << n;
+        return 0;
+    }
+    while(1){
+        if(temp/10!=0){
+            cnt += a[idx]*idx;
+            temp /= 10;
+            idx++;
+        }
+        else{
+            int sum=0;
+            for(int j=1; j<idx; j++)
+                sum+=a[j];
+            cnt += (n-sum)*idx;
+            break;
         }
     }
-    cout << MAX;
+    cout << cnt;
 }
