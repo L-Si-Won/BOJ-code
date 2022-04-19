5d8ef26c0eeca0c8344535a71859c2aae42c96a8
SiWon Lee <tldnjs0821222@naver.com>
2022-04-19 22:04:19 +0900

    15654

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int n, m;
 int arr[9];
+int temp[9];
 bool visit[9];
 
-void dfs(int num, int cnt){
+void dfs(int cnt){
     if(cnt==m){
         for(int i=0; i<m; i++)
-            cout << arr[i] << " ";
+            cout << temp[i] << " ";
         cout << "\n";
         return ;
     }
-    for(int i=num; i<n+1; i++){
-            arr[cnt]=i;
-            dfs(i, cnt+1);
+
+    for(int i=1; i<n+1; i++){
+        if(!visit[i]){
+            visit[i]=true;
+            temp[cnt]=arr[i];
+            dfs(cnt+1);
+            visit[i]=false;
+        }
     }
 }
 
@@ -25,5 +32,8 @@ int main(){
     ios_base::sync_with_stdio(false);
 
     cin >> n >> m;
-    dfs(1, 0);
+    for(int i=1; i<n+1; i++)
+        cin >> arr[i];
+    sort(arr+1, arr+n+1);
+    dfs(0);
 }
