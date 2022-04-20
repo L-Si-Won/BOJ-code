cd0d0d63a846b8e36e5423159f1a40b018df61d8
SiWon Lee <tldnjs0821222@naver.com>
2022-04-20 15:18:48 +0900

    15656

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -8,7 +8,7 @@ int arr[9];
 int temp[9];
 bool visit[9];
 
-void dfs(int num, int cnt){
+void dfs(int cnt){
     if(cnt==m){
         for(int i=0; i<m; i++)
             cout << temp[i] << " ";
@@ -16,13 +16,9 @@ void dfs(int num, int cnt){
         return ;
     }
 
-    for(int i=num; i<n+1; i++){
-        if(!visit[i]){
-            visit[i]=true;
-            temp[cnt]=arr[i];
-            dfs(i+1, cnt+1);
-            visit[i]=false;
-        }
+    for(int i=1; i<n+1; i++){
+        temp[cnt]=arr[i];
+        dfs(cnt+1);
     }
 }
 
@@ -35,5 +31,5 @@ int main(){
     for(int i=1; i<n+1; i++)
         cin >> arr[i];
     sort(arr+1, arr+n+1);
-    dfs(1, 0);
+    dfs(0);
 }
