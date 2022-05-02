05f76ae995c564627af3293e3dccbdfad28cb0d2
SiWon Lee <tldnjs0821222@naver.com>
2022-05-02 18:40:41 +0900

    10819

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -6,20 +6,11 @@ using namespace std;
 int n;
 int arr[9];
 
-bool comp(int i, int j){
-    return i > j;
-}
-
-bool dfs(){
-    int i = n-1;
-    while(i>0 && arr[i-1]>arr[i]) i--;
-    if(i<=0) return false;
-
-    int j=n-1;
-    while(arr[i-1]>arr[j]) j--;
-    swap(arr[i-1], arr[j]);
-    sort(arr+i, arr+n);
-    return true;
+int sum(){
+    int sum=0;
+    for(int i=0; i<n-1; i++)
+        sum += abs(arr[i]-arr[i+1]);
+    return sum;
 }
 
 int main(){
@@ -29,14 +20,12 @@ int main(){
 
     cin >> n;
     for(int i=0; i<n; i++)
-        arr[i]=i+1;
-    
-    for(int i=0; i<n; i++)
-        cout << arr[i] << " ";
-    cout << "\n";
-    while(dfs()){
-        for(int i=0; i<n; i++)
-            cout << arr[i] << " ";
-        cout << "\n";
-    }
+        cin >> arr[i];
+
+    sort(arr, arr+n);
+
+    int MAX=sum();
+    while(next_permutation(arr, arr+n))
+        MAX=max(MAX, sum());
+    cout << MAX;
 }
