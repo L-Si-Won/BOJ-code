008ddbda94604f08089a7086d5b34093913e7075
SiWon Lee <tldnjs0821222@naver.com>
2022-05-06 16:48:50 +0900

    14889

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -4,27 +4,57 @@
 using namespace std;
 
 int n;
-int t[15];
-int p[15];
-int d[15][2];
-int MAX;
+int arr[20][20];
+int MIN=2e9;
+bool visit[20];
+int temp[10];
 
-int dp(){
-    if(t[0]<=n) d[0][1]=p[0];
+void solve(){
+    int sum1=0;
+    int sum2=0;
 
-    for(int i=1; i<n; i++){
-        d[i][0]=max(d[i-1][0], d[i-1][1]);
-        
-        if(i+t[i]<=n){
-            d[i][1]=p[i];
-            for(int j=i-1; j>=0; j--){
-                if(i-j>=t[j])
-                    d[i][1]=max(d[i][1], d[j][1]+p[i]);
+    for(int i=0; i<n/2; i++){
+        for(int j=0; j<n/2; j++){
+            if(i!=j){
+                sum1 += arr[temp[i]][temp[j]];
             }
         }
     }
-    MAX=max(d[n-1][0], d[n-1][1]);
-    return MAX;
+
+    int temp2[10];
+    int k=0;
+    for(int i=0; i<n; i++){
+        if(visit[i]==false){
+            temp2[k]=i;
+            k++;
+        }
+    }
+    
+    for(int i=0; i<n/2; i++){
+        for(int j=0; j<n/2; j++){
+            if(i!=j){
+                sum2 += arr[temp2[i]][temp2[j]];
+            }
+        }
+    }
+
+    MIN=min(MIN, abs(sum1-sum2));
+}
+
+void dfs(int cnt, int num){
+    if(cnt==n/2){
+        solve();
+        return ;
+    }
+
+    for(int i=num; i<n; i++){
+        if(visit[i]==false){
+            visit[i]=true;
+            temp[cnt]=i;
+            dfs(cnt+1, i+1);
+            visit[i]=false;
+        }
+    }
 }
 
 int main(){
@@ -34,7 +64,10 @@ int main(){
 
     cin >> n;
     for(int i=0; i<n; i++)
-        cin >> t[i] >> p[i];
-
-    cout << dp();
+        for(int j=0; j<n; j++)
+            cin >> arr[i][j];
+    
+    visit[0]=true;
+    dfs(1, 1); //temp[0]에 이미 0이 들어가있다고 생각->0은 무조건 포함
+    cout << MIN;
 }
