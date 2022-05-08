001a1a93defed627acef5b560af6ba3c7feccd88
SiWon Lee <tldnjs0821222@naver.com>
2022-05-08 17:25:45 +0900

    2529

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,57 +1,74 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
-int n;
-int arr[20][20];
-int MIN=2e9;
-bool visit[20];
-int temp[20];
+int k;
+char arr[9];
+int a[10]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int temp[10];
+bool visit[10];
+int MIN[10]={10,10,10,10,10,10,10,10,10,10};
+int MAX[10];
 
-void solve(int team){
-    int sum1=0;
-    int sum2=0;
 
-    for(int i=0; i<team; i++){
-        for(int j=0; j<team; j++){
-            if(i!=j){
-                sum1 += arr[temp[i]][temp[j]];
-            }
+bool check(){
+    for(int i=0; i<k; i++){
+        if(arr[i]=='>'){
+            if(temp[i] > temp[i+1])
+                continue;
+            else
+                return false;
+        }
+        else if(arr[i]=='<'){
+            if(temp[i] < temp[i+1])
+                continue;
+            else
+                return false;
         }
     }
-
-    int temp2[20];
-    int k=0;
-    for(int i=0; i<n; i++){
-        if(visit[i]==false){
-            temp2[k]=i;
-            k++;
-        }
-    }
-    
-    for(int i=0; i<n-team; i++){
-        for(int j=0; j<n-team; j++){
-            if(i!=j){
-                sum2 += arr[temp2[i]][temp2[j]];
-            }
-        }
-    }
-
-    MIN=min(MIN, abs(sum1-sum2));
+    return true;
 }
 
-void dfs(int cnt, int num, int team){
-    if(cnt==team){
-        solve(team);
+void Min_Max(){
+    int i=0;
+    while(1){
+        if(MIN[i]>temp[i]){
+            for(int j=0; j<k+1; j++)
+                MIN[j]=temp[j];
+            break;
+        }
+        else if(MIN[i]==temp[i])
+            i++;
+        else
+            break;
+    }
+    i=0;
+    while(1){
+        if(MAX[i]<temp[i]){
+            for(int j=0; j<k+1; j++)
+                MAX[j]=temp[j];
+            break;
+        }
+        else if(MAX[i]==temp[i])
+            i++;
+        else
+            break;
+    }
+}
+
+void solve(int cnt){
+    if(cnt==k+1){
+        if(check())
+            Min_Max();
         return ;
     }
-
-    for(int i=num; i<n; i++){
+    for(int i=0; i<10; i++){
         if(visit[i]==false){
             visit[i]=true;
-            temp[cnt]=i;
-            dfs(cnt+1, i+1, team);
+            temp[cnt]=a[i];
+            solve(cnt+1);
             visit[i]=false;
         }
     }
@@ -62,13 +79,16 @@ int main(){
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            cin >> arr[i][j];
+    cin >> k;
+    for(int i=0; i<k; i++)
+        cin >> arr[i];
+    solve(0);
     
-    visit[0]=true;
-    for(int i=1; i<=n-1; i++)
-        dfs(1, 1, i);
-    cout << MIN;
+    string s_max;
+    string s_min;
+    for(int i=0; i<k+1; i++){
+        s_max+=to_string(MAX[i]);
+        s_min+=to_string(MIN[i]);
+    }
+    cout << s_max << "\n" << s_min;
 }
