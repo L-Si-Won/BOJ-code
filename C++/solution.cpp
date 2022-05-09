07c409dc504ea28868cfd68538efced695b4e36a
SiWon Lee <tldnjs0821222@naver.com>
2022-05-09 18:49:53 +0900

    11723_2

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
 
 using namespace std;
 
 int n;
 string cmd;
 int x;
-int bit;
+vector<int> v;
 
 int main(){
     cin.tie(NULL);
@@ -16,25 +19,62 @@ int main(){
     for(int i=0; i<n; i++){
         cin >> cmd;
         if(cmd=="all" || cmd=="empty"){
-            if(cmd=="all")
-                bit=(1<<21)-1;
+            if(cmd=="all"){
+                v.clear();
+                for(int j=1; j<21; j++)
+                    v.push_back(j);
+            }
             if(cmd=="empty")
-                bit=0;
+                v.clear();
         }
         else{
             cin >> x;
-            if(cmd=="add")
-                bit=bit|(1<<x);
-            if(cmd=="remove")
-                bit=bit&~(1<<x);
-            if(cmd=="check"){
-                if(bit&(1<<x))
-                    cout << "1\n";
-                else   
-                    cout << "0\n";
+            if(cmd=="add"){
+                int check=0;
+                for(int j=0; j<v.size(); j++)
+                    if(v[j]==x)
+                        check++;
+                if(check==0)
+                    v.push_back(x);
             }
-            if(cmd=="toggle")
-                bit=bit^(1<<x);
+            if(cmd=="remove"){
+                int temp=0;
+                for(int j=0; j<v.size(); j++){
+                    if(v[j]==x){
+                        v[j]=0;
+                        temp=j;
+                        break;
+                    }
+                }
+                v.erase(v.begin()+temp, v.begin()+temp);
+            }
+            if(cmd=="check"){
+                int check=0;
+                for(int j=0; j<v.size(); j++)
+                    if(v[j]==x)
+                        check++;
+                if(check>0) cout << "1\n";
+                else cout << "0\n";
+            }
+            if(cmd=="toggle"){
+                int check=0;
+                int temp=0;
+                for(int j=0; j<v.size(); j++){
+                    if(v[j]==x){
+                        v[j]=0;
+                        temp=j;
+                        check++;
+                        break;
+                    }
+                }
+                if(check>0){
+                    for(int j=temp; j<v.size(); j++)
+                        v[j]=v[j+1];
+                    v.resize(v.size()-1);
+                }
+                if(check==0)
+                    v.push_back(x);
+            }
         }
     }
 }
