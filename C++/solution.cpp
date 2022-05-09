ffb6ec8626e224fa8f6f8700335c5ddd4ba9ae63
SiWon Lee <tldnjs0821222@naver.com>
2022-05-09 14:37:47 +0900

    11723

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,94 +1,40 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
 
 using namespace std;
 
-int k;
-char arr[9];
-int a[10]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-int temp[10];
-bool visit[10];
-int MIN[10]={10,10,10,10,10,10,10,10,10,10};
-int MAX[10];
-
-
-bool check(){
-    for(int i=0; i<k; i++){
-        if(arr[i]=='>'){
-            if(temp[i] > temp[i+1])
-                continue;
-            else
-                return false;
-        }
-        else if(arr[i]=='<'){
-            if(temp[i] < temp[i+1])
-                continue;
-            else
-                return false;
-        }
-    }
-    return true;
-}
-
-void Min_Max(){
-    int i=0;
-    while(1){
-        if(MIN[i]>temp[i]){
-            for(int j=0; j<k+1; j++)
-                MIN[j]=temp[j];
-            break;
-        }
-        else if(MIN[i]==temp[i])
-            i++;
-        else
-            break;
-    }
-    i=0;
-    while(1){
-        if(MAX[i]<temp[i]){
-            for(int j=0; j<k+1; j++)
-                MAX[j]=temp[j];
-            break;
-        }
-        else if(MAX[i]==temp[i])
-            i++;
-        else
-            break;
-    }
-}
-
-void solve(int cnt){
-    if(cnt==k+1){
-        if(check())
-            Min_Max();
-        return ;
-    }
-    for(int i=0; i<10; i++){
-        if(visit[i]==false){
-            visit[i]=true;
-            temp[cnt]=a[i];
-            solve(cnt+1);
-            visit[i]=false;
-        }
-    }
-}
+int n;
+string cmd;
+int x;
+int bit;
 
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> k;
-    for(int i=0; i<k; i++)
-        cin >> arr[i];
-    solve(0);
-    
-    string s_max;
-    string s_min;
-    for(int i=0; i<k+1; i++){
-        s_max+=to_string(MAX[i]);
-        s_min+=to_string(MIN[i]);
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> cmd;
+        if(cmd=="all" || cmd=="empty"){
+            if(cmd=="all")
+                bit=(1<<21)-1;
+            if(cmd=="empty")
+                bit=0;
+        }
+        else{
+            cin >> x;
+            if(cmd=="add")
+                bit=bit|(1<<x);
+            if(cmd=="remove")
+                bit=bit&~(1<<x);
+            if(cmd=="check"){
+                if(bit&(1<<x))
+                    cout << "1\n";
+                else   
+                    cout << "0\n";
+            }
+            if(cmd=="toggle")
+                bit=bit^(1<<x);
+        }
     }
-    cout << s_max << "\n" << s_min;
 }
