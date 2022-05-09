df0656ea6e87398fa5792566033865ae34957297
SiWon Lee <tldnjs0821222@naver.com>
2022-05-09 19:20:58 +0900

    1182

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,80 +1,42 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <vector>
 
 using namespace std;
 
 int n;
-string cmd;
-int x;
-vector<int> v;
+int s;
+int arr[20];
+int temp[20];
+bool visit[20];
+int a;
+
+void dfs(int cnt, int num, int b){
+    if(cnt==b){
+        int sum=0;
+        for(int i=0; i<n; i++)
+            sum += temp[i];
+        if(sum==s) a++;
+        return ;
+    }
+
+    for(int i=num; i<n; i++){
+        if(visit[i]==false){
+            visit[i]=true;
+            temp[cnt]=arr[i];
+            dfs(cnt+1, i+1, b);
+            visit[i]=false;
+        }
+    }
+}
 
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    for(int i=0; i<n; i++){
-        cin >> cmd;
-        if(cmd=="all" || cmd=="empty"){
-            if(cmd=="all"){
-                v.clear();
-                for(int j=1; j<21; j++)
-                    v.push_back(j);
-            }
-            if(cmd=="empty")
-                v.clear();
-        }
-        else{
-            cin >> x;
-            if(cmd=="add"){
-                int check=0;
-                for(int j=0; j<v.size(); j++)
-                    if(v[j]==x)
-                        check++;
-                if(check==0)
-                    v.push_back(x);
-            }
-            if(cmd=="remove"){
-                int temp=0;
-                for(int j=0; j<v.size(); j++){
-                    if(v[j]==x){
-                        v[j]=0;
-                        temp=j;
-                        break;
-                    }
-                }
-                v.erase(v.begin()+temp, v.begin()+temp);
-            }
-            if(cmd=="check"){
-                int check=0;
-                for(int j=0; j<v.size(); j++)
-                    if(v[j]==x)
-                        check++;
-                if(check>0) cout << "1\n";
-                else cout << "0\n";
-            }
-            if(cmd=="toggle"){
-                int check=0;
-                int temp=0;
-                for(int j=0; j<v.size(); j++){
-                    if(v[j]==x){
-                        v[j]=0;
-                        temp=j;
-                        check++;
-                        break;
-                    }
-                }
-                if(check>0){
-                    for(int j=temp; j<v.size(); j++)
-                        v[j]=v[j+1];
-                    v.resize(v.size()-1);
-                }
-                if(check==0)
-                    v.push_back(x);
-            }
-        }
-    }
+    cin >> n >> s;
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    for(int i=1; i<n+1; i++)
+        dfs(0 ,0, i);
+    cout << a;
 }
