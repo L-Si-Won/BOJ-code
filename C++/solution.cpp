9f19d4259bc77ce00cb3d897d8778efa612df0a1
SiWon Lee <tldnjs0821222@naver.com>
2022-05-11 16:46:03 +0900

    2579

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,64 +1,29 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
-using namespace std;
-int arr[4][4];
-bool check[4][4] = { false, };
-int row, col, result = 0;
 
-void brute(int num, int sum) {
-	int x = num % col;
-	int y = num / col;
-	if (num >= row * col) {
-		result = max(result, sum);
-		return;
-	}
-	if (check[y][x]) brute(num + 1, sum);
-	else {
-		int val, ny, nx;
-		val = arr[y][x];
-		check[y][x] = true;
-		brute(num + 1, sum + val);
-		check[y][x] = false;
-		//밑으로
-		for (int k = 1; k < row - y; k++) {
-			nx = x;
-			ny = y + k;
-			val *= 10;
-			val += arr[ny][nx];
-			for (int j = 1; j <= k; j++)
-				check[y + j][nx] = true;
-			brute(num + 1, sum + val);
-			for (int j = 1; j <= k; j++)
-				check[y + j][nx] = false;
-		}
-		val = arr[y][x];
-		//오른쪽으로
-		for (int k = 1; k < col - x; k++) {
-			ny = y;
-			nx = x + k;
-			if (check[ny][nx]) return;
-			val *= 10;
-			val += arr[ny][nx];
-			for (int j = 1; j <= k; j++)
-				check[ny][x+j] = true;
-			brute(num + 1, sum + val);
-			for (int j = 1; j <= k; j++)
-				check[ny][x + j] = false;
-		}
-	}
+using namespace std;
+
+int n;
+int arr[301];
+int d[301];
+
+int dp(){
+    d[1]=arr[1];
+    d[2]=arr[1]+arr[2];
+    d[3]=max(arr[1]+arr[3], arr[2]+arr[3]);
+
+    for(int i=3; i<n+1; i++)
+        d[i]=max(arr[i]+arr[i-1]+d[i-3], arr[i]+d[i-2]);
+    return d[n];
 }
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> row >> col;
-	string str;
-	for (int i = 0; i < row; i++) {
-		cin >> str;
-		for (int j = 0; j < col; j++)
-			arr[i][j] = str[j] - '0';
-	}
-	brute(0, 0);
-	cout << result;
-	return 0;
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    cin >> n;
+    for(int i=1; i<n+1; i++)
+        cin >> arr[i];
+    cout << dp();
 }
