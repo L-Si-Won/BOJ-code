57bc99f730010bb1da1c1bf092e7fe599ef1500f
SiWon Lee <tldnjs0821222@naver.com>
2022-05-14 19:06:49 +0900

    2667

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,82 +1,63 @@
 #include <iostream>
-#include <vector>
-#include <cstring>
-#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-#define R 1
-#define B 2
-int v, e, k;
-vector<int> arr[20001];
-int visit[20001];
+int n;
+int arr[27][27];
+bool visit[27][27];
+int cont[1000];
+int count_idx;
 
-void dfs(int cur){
-    if(!visit[cur]) visit[cur]=R;
-    for(int i=0; i<arr[cur].size(); i++){
-        int next=arr[cur][i];
-        if(visit[next]==false){
-            if(visit[cur]==R) visit[next]=B;
-            else if(visit[cur]==B) visit[next]=R;
-            dfs(next);
+void dfs(int x, int y){
+    visit[x][y]=true;
+    cont[count_idx]++;
+    int cnt=4;
+    while(cnt--){
+        switch(cnt){
+            case 3:
+                if(arr[x-1][y]==1 && visit[x-1][y]==false)
+                    dfs(x-1, y);
+                break;
+            case 2:
+                if(arr[x][y-1]==1 && visit[x][y-1]==false)
+                    dfs(x, y-1);
+                break;
+            case 1:
+                if(arr[x][y+1]==1 && visit[x][y+1]==false)
+                    dfs(x, y+1);
+                break;
+            case 0:
+                if(arr[x+1][y]==1 && visit[x+1][y]==false)
+                    dfs(x+1, y);
+                break;
         }
     }
-}
-
-void bfs(int cur){
-    queue<int> q;
-    q.push(cur);
-    visit[cur]=true;
-    while(!q.empty()){
-        int c=q.front();
-        q.pop();
-        cout << c << " ";
-        for(int i=0; i<arr[c].size(); i++){
-            int next=arr[c][i];
-            if(visit[next]==false){
-                visit[next]=true;
-                q.push(next);
-            }
-        }
-    }
-}
-
-void check(){
-    for(int i=1; i<v+1; i++){
-        for(int j=0; j<arr[i].size(); j++){
-            if(visit[i]==visit[arr[i][j]]){
-                cout << "NO\n";
-                return ;
-            }
-        }
-    }
-    cout << "YES\n";
 }
 
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
 
-    cin >> k;
-    for(int i=0; i<k; i++){
-        cin >> v >> e;
-        for(int j=0; j<e; j++){
-            int a, b;
-            cin >> a >> b;
-            arr[a].push_back(b);
-            arr[b].push_back(a);
-        }
-        
-        for(int j=1; j<v+1; j++)
-            if(!visit[j])
-                dfs(j);
-
-        check();
-
-        for(int j=0; j<v+1; j++)
-            arr[j].clear();
-        memset(visit, 0, sizeof(visit));
+    cin >> n;
+    for(int i=1; i<n+1; i++){
+        string s;
+        cin >> s;
+        for(int j=1; j<n+1; j++)
+            arr[i][j]=s[j-1]-'0';
     }
+    
+    for(int i=1; i<n+1; i++){
+        for(int j=1; j<n+1; j++){
+            if(visit[i][j]==false && arr[i][j]==1){
+                dfs(i, j);
+                count_idx++;
+            }
+        }
+    }
+    sort(cont, cont+count_idx);
+    cout << count_idx << "\n";
+    for(int i=0; i<count_idx; i++)
+        cout << cont[i] << "\n";
 }
