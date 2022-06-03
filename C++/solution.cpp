6b5631afd09d8ea28783c9cc8eb7fbdc7715f9a6
SiWon Lee <tldnjs0821222@naver.com>
2022-06-03 19:41:52 +0900

    2573

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -4,53 +4,84 @@
 
 using namespace std;
 
-int n;
-int arr[100][100]; //지도
-bool visit[100][100]; //방문확인
-bool fall[100][100]; //잠겼는지 확인
-int MAX; //영역 최대 개수
+int n, m; //행 열
+int arr[300][300];
+int temp_arr[300][300];
+bool visit[300][300];
 int dx[]={1, -1, 0, 0};
 int dy[]={0, 0, 1, -1};
 
-void bfs(int x, int y){
+void ice_cnt(int x, int y){
     queue<pair<int, int>> q;
     q.push({x, y});
     visit[x][y]=true;
 
     while(!q.empty()){
-        int cur_x=q.front().first;
-        int cur_y=q.front().second;
+        int cx=q.front().first;
+        int cy=q.front().second;
         q.pop();
 
         for(int i=0; i<4; i++){
-            int nx=cur_x+dx[i];
-            int ny=cur_y+dy[i];
-            if(nx>=0 && ny>=0 && nx<n && ny<n){
-                if(visit[nx][ny]==false && fall[nx][ny]==false){
-                    q.push({nx, ny});
+            int nx=cx+dx[i];
+            int ny=cy+dy[i];
+            
+            if(nx>=0 && ny>=0 && nx<n && ny<m){
+                if(visit[nx][ny]==false && arr[nx][ny]!=0){
                     visit[nx][ny]=true;
+                    q.push({nx, ny});
                 }
             }
         }
     }
 }
 
-int solve(int stair){
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            if(arr[i][j]<=stair)
-                fall[i][j]=true;
-
-    int temp_max=0;
+void ice_melt(){ //동시에 녹는 것을 표현하기 위해 temp_arr사용
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(visit[i][j]==false && fall[i][j]==false){
-                bfs(i, j);
-                temp_max++;
+        for(int j=0; j<m; j++){
+            if(arr[i][j]!=0){
+                int temp=arr[i][j];
+                for(int k=0; k<4; k++){
+                    int cx=i+dx[k];
+                    int cy=j+dy[k];
+                    if(cx>=0 && cy>=0 && cx<n && cy<m)
+                        if(arr[cx][cy]==0)
+                            temp--;
+                }
+                if(temp<0) temp=0;
+                temp_arr[i][j]=temp;
             }
+            else temp_arr[i][j]=0;
         }
     }
-    return temp_max;
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            arr[i][j]=temp_arr[i][j];
+}
+
+void solve(){
+    int year=0;
+    while(1){
+        int ice=0; //year년 후 빙산 개수
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(visit[i][j]==false && arr[i][j]!=0){
+                    ice_cnt(i, j);
+                    ice++;
+                }
+            }
+        }
+        if(ice==0){//다 녹았을 경우
+            cout << "0";
+            break;
+        }
+        if(ice>=2){
+            cout << year;
+            break;
+        }
+        memset(visit, false, sizeof(visit));
+        ice_melt();
+        year++;
+    }
 }
 
 int main(){
@@ -58,16 +89,9 @@ int main(){
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
+    cin >> n >> m;
     for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
+        for(int j=0; j<m; j++)
             cin >> arr[i][j];
-    for(int i=0; i<=99; i++){
-        int sol=solve(i);
-        if(sol==0) break;
-        MAX=max(MAX, sol);
-        memset(visit, false, sizeof(visit));
-        memset(fall, false, sizeof(fall));
-    }
-    cout << MAX;
+    solve();
 }
