b18a8b72d281e08b4645315c7f395d795ca249f4
SiWon Lee <tldnjs0821222@naver.com>
2022-06-05 14:06:20 +0900

    14503

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,63 +1,62 @@
 #include <iostream>
 #include <queue>
 #include <cstring>
+#include <tuple>
 
 using namespace std;
 
-int t, n;
-pair<int, int> home, store[100], rock; //열 행
-bool visit[100]; //편의점 방문여부
+int n, m; //행 열, 좌표(n,m)
+int arr[50][50];
+bool visit[50][50];
+int clear_cnt;
+int dx[]={-1, 0, 1, 0};
+int dy[]={0, 1, 0, -1};
+int r, c, d;
 
-void solve(){
-    if(abs(home.first-rock.first)+abs(home.second-rock.second)<=1000){
-        cout << "happy\n";
-        return ;
-    }
+void dfs(){
+    for(int i=0; i<4; i++){
+        int nd=(d+3-i)%4;
+        int nr=r+dx[nd];
+        int nc=c+dy[nd];
 
-    queue<pair<int, int>> q;
-    q.push(home);
-
-    while(!q.empty()){
-        pair<int, int> cur=q.front();
-        q.pop();
-
-        if(cur==home){
-            for(int i=0; i<n; i++){
-                if(visit[i]==false && (abs(cur.first-store[i].first)+abs(cur.second-store[i].second)<=1000)){
-                    q.push(store[i]);
-                    visit[i]=true;
-                }
-            }
-        }
-        else{
-            if(abs(cur.first-rock.first)+abs(cur.second-rock.second)<=1000){
-                cout << "happy\n";
-                return ;
-            }
-            for(int i=0; i<n; i++){
-                if(visit[i]==false && (abs(cur.first-store[i].first)+abs(cur.second-store[i].second)<=1000)){
-                    q.push(store[i]);
-                    visit[i]=true;
-                }
-            }
+        if(arr[nr][nc]==1) continue;
+        if(arr[nr][nc]==0 && visit[nr][nc]==false){
+            visit[nr][nc]=true;
+            r=nr;
+            c=nc;
+            d=nd;
+            clear_cnt++;
+            dfs();
         }
     }
-    cout << "sad\n";
+
+    int bd=d>1?d-2:d+2;
+    int br=r+dx[bd];
+    int bc=c+dy[bd];
+
+    if(arr[br][bc]==0){
+        r=br;
+        c=bc;
+        dfs();
+    }
+    else{
+        cout << clear_cnt;
+        exit(0);
+    }
 }
+
 
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> t;
-    for(int i=0; i<t; i++){
-        cin >> n;
-        cin >> home.first >> home.second;
-        for(int j=0; j<n; j++)
-            cin >> store[j].first >> store[j].second;
-        cin >> rock.first >> rock.second;
-        solve();
-        memset(visit, false, sizeof(visit));
-    }
+    cin >> n >> m;
+    cin >> r >> c >>d;
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            cin >> arr[i][j];
+    visit[r][c]=true;
+    clear_cnt++;
+    dfs();
 }
