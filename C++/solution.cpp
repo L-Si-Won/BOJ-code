94ffeae99aad7ff36e9cacc92c54ada900864f49
SiWon Lee <tldnjs0821222@naver.com>
2022-06-21 08:10:36 +0900

    2294

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int arr[500][500];
-int m, n, h; //행 열
-bool visit[500][500];
-int dx[]={0, 0, 1, -1};
-int dy[]={1, -1, 0, 0};
-int dp[500][500];
+int n, k;
+int coin[101];
+int dp[10001];
 
-int solve(int x, int y){
-    if(x==m-1 && y==n-1) return 1;
-    if(visit[x][y]==true) return dp[x][y];
-    visit[x][y]=true;
-    for(int i=0; i<4; i++){
-        int nx=x+dx[i];
-        int ny=y+dy[i];
-        if(nx>=0 && ny>=0 && nx<m && ny<n)
-            if(arr[nx][ny]<arr[x][y])
-                dp[x][y]+=solve(nx, ny);
+void solve(){
+    for(int i=1; i<=k; i++)
+        dp[i]=2e9;
+
+    for(int i=1; i<=n; i++){
+        for(int j=coin[i]; j<=k; j++){
+            dp[j]=min(dp[j], dp[j-coin[i]]+1);
+        }
     }
-    return dp[x][y];
+
+    if(dp[k]==2e9) cout << "-1";
+    else cout << dp[k];
 }
 
 int main(){
@@ -28,9 +26,8 @@ int main(){
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> m >> n;
-    for(int i=0; i<m; i++)
-        for(int j=0; j<n; j++)
-            cin >> arr[i][j];
-    cout << solve(0, 0);
+    cin >> n >> k;
+    for(int i=1; i<=n; i++)
+        cin >> coin[i];
+    solve();
 }
