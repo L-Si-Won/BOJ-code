0b1440d128cbe37487dc3940490697b3e37135d6
SiWon Lee <tldnjs0821222@naver.com>
2022-06-21 12:20:25 +0900

    2293

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -8,17 +8,11 @@ int coin[101];
 int dp[10001];
 
 void solve(){
-    for(int i=1; i<=k; i++)
-        dp[i]=2e9;
-
-    for(int i=1; i<=n; i++){
-        for(int j=coin[i]; j<=k; j++){
-            dp[j]=min(dp[j], dp[j-coin[i]]+1);
-        }
-    }
-
-    if(dp[k]==2e9) cout << "-1";
-    else cout << dp[k];
+    dp[0]=1;
+    for(int i=1; i<=n; i++)
+        for(int j=coin[i]; j<=k; j++)
+            dp[j]+=dp[j-coin[i]];
+    cout << dp[k];
 }
 
 int main(){
