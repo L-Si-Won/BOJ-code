fd386ba74c46b876c824a0ffdce7efd4f2b2728b
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-06-27 12:50:00 +0900

    12865

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,60 +1,30 @@
 #include <iostream>
-#include <cstring>
+#include <map>
 
 using namespace std;
 
-int n;
-char arr[100][100];
-int garo, sero;
-bool visit[100][100];
+int n, k;
+int w[101];
+int v[101];
+int dp[101][100001];
+
+void solve(){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=k; j++){
+            if(j-w[i]>=0) dp[i][j]=max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]);
+            else dp[i][j]=dp[i-1][j];
+        }
+    }
+    cout << dp[n][k];
+}
 
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    for(int i=0; i<n; i++){
-        string s;
-        cin >> s;
-        for(int j=0; j<n; j++)
-            arr[i][j]=s[j];
-    }
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(visit[i][j]==false){
-                if(arr[i][j]=='.'){
-                    visit[i][j]=true;
-                    j++;
-                    int check=0;
-                    while(j<n && arr[i][j]=='.'){
-                        visit[i][j]=true;
-                        j++;
-                        check++;
-                    }
-                    if(check!=0) garo++;
-                }
-            }
-        }
-    }
-    memset(visit, false, sizeof(visit));
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(visit[j][i]==false){
-                if(arr[j][i]=='.'){
-                    visit[j][i]=true;
-                    j++;
-                    int check=0;
-                    while(j<n && arr[j][i]=='.'){
-                        visit[j][i]=true;
-                        j++;
-                        check++;
-                    }
-                    if(check!=0) sero++;
-                }
-            }
-        }
-    }
-    cout << garo << " " << sero;
+    cin >> n >> k;
+    for(int i=1; i<=n; i++)
+        cin >> w[i] >> v[i];
+    solve();
 }
