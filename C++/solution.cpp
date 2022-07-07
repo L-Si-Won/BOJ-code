a3955d983171828e8e54655a986cec92f62fde0e
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-07-07 09:58:46 +0900

    11049

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int t, k, arr[501];
-int dp[501][501], sum[501];
+int n;
+vector<pair<int, int>> v; //행, 열
+int dp[501][501];
 
 void solve(){
-    for(int i=1; i<k+1; i++)
-        sum[i]=sum[i-1]+arr[i];
-
-    for(int i=1; i<k+1; i++){
-        for(int j=1; j<=k-i; j++){
-            dp[j][i+j]=2e9;
-            for(int l=j; l<i+j; l++)
-                dp[j][i+j]=min(dp[j][i+j], dp[j][l]+dp[l+1][i+j]+sum[i+j]-sum[j-1]);
+    for(int i=1; i<n; i++){
+        for(int j=1; j<=n-i; j++){
+            dp[j][i+j]=2147483647; //INT_MAX도 가능
+            for(int k=j; k<=i+j; k++)
+                dp[j][i+j]=min(dp[j][i+j], dp[j][k]+dp[k+1][i+j]+(v[j].first*v[k].second*v[i+j].second));
         }
     }
-    cout << dp[1][k] << "\n";
+    cout << dp[1][n];
 }
 
 int main(){
@@ -25,11 +24,12 @@ int main(){
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> t;
-    while(t--){
-        cin >> k;
-        for(int i=1; i<k+1; i++)
-            cin >> arr[i];
-        solve();
+    cin >> n;
+    v.push_back({0, 0});
+    for(int i=0; i<n; i++){
+        int a, b;
+        cin >> a >> b;
+        v.push_back({a, b});
     }
+    solve();
 }
