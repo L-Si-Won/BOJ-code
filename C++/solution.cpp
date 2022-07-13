ea67b646361f1a4de38cca095c664919cf4d2132
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-07-13 16:23:41 +0900

    1039

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
-int n, m, arr[2001], s, e;
-int dp[2001][2001];
+int k;
+string n;
 
 void solve(){
-    for(int i=1; i<=n; i++)
-        dp[i][i]=1;
-    for(int i=1; i<n; i++)
-        if(arr[i]==arr[i+1]) dp[i][i+1]=1;
+    queue<string> q;
+    q.push(n);
+    int MAX=0;
 
-    for(int i=n-1; i>=1; i--)
-        for(int j=i+2; j<=n; j++)
-            dp[i][j] = (dp[i+1][j-1]==1 && arr[i]==arr[j]) ? 1 : 0;
+    for(int i=0; i<k; i++){
+        set<string> visit;
+        int qsize=q.size();
+
+        for(int j=0; j<qsize; j++){ //q.size는 매번 변경되므로 변수로 고정해야함
+            string cur=q.front();
+            q.pop();
+
+            for(int l=0; l<cur.size()-1; l++){
+                for(int r=l+1; r<cur.size(); r++){
+                    if(l==0 && cur[r]=='0') continue; //0으로 시작하는 수 예외 처리
+
+                    swap(cur[l], cur[r]);
+                    if(visit.find(cur)==visit.end()){ //새로운 수일 경우
+                        if(i==k-1){ //k번째 교환 단계일 경우
+                            MAX=max(MAX, stoi(cur));
+                        }
+                        q.push(cur);
+                        visit.insert(cur);
+                    }
+                    swap(cur[l], cur[r]);
+                }
+            }
+        }
+    }
+    cout << MAX;
 }
 
 int main(){
@@ -21,13 +47,10 @@ int main(){
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    for(int i=1; i<=n; i++)
-        cin >> arr[i];
-    solve();
-    cin >> m;
-    for(int i=0; i<m; i++){
-        cin >> s >> e;
-        cout << dp[s][e] << "\n";
+    cin >> n >> k;
+    if(n.size()==1 || (n.size()==2 && stoi(n)%10==0)){
+        cout << -1;
+        return 0;
     }
+    solve();
 }
