45c29e16541959ff078fcb779db3c79aea7a03f0
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-07-26 12:29:05 +0900

    2621

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,50 +1,18 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <cstring>
 #include <algorithm>
 
 using namespace std;
 
-int T, n, m, t, s, g, h, a, b, d;
-vector<pair<int, int>> v[2001]; //도로(교차로, 거리)
-vector<int> dist; //목적지 후보
-vector<int> ans;
-int S[2001], G[2001], H[2001];
+pair<char, int> card[5];
+vector<int> score;
 
-void dijkstra(int start, int A[2001]){
-    queue<pair<int, int>> q; //거리, 교차로
-    q.push({0, start});
-    A[start]=0;
-
-    while(!q.empty()){
-        int cost=q.front().first;
-        int cur=q.front().second;
-        q.pop();
-
-        for(int i=0; i<v[cur].size(); i++){
-            int next=v[cur][i].first;
-            int ncost=v[cur][i].second;
-            if(A[next]>cost+ncost){
-                A[next]=cost+ncost;
-                q.push({A[next], next});
-            }
-        }
-    }
+bool comp(pair<char, int> a, pair<char, int> b){
+    return a.second < b.second ? true : false;
 }
 
-void solve(){
-    dijkstra(s, S);
-    dijkstra(g, G);
-    dijkstra(h, H);
-    for(int i=0; i<dist.size(); i++){
-        if(S[dist[i]]==S[g]+G[h]+H[dist[i]]) ans.push_back(dist[i]);
-        else if(S[dist[i]]==S[h]+H[g]+G[dist[i]]) ans.push_back(dist[i]);
-    }
-    sort(ans.begin(), ans.end());
-    for(int i=0; i<ans.size(); i++)
-        cout << ans[i] << " ";
-    cout << "\n";
+bool comp2(pair<int, int> a, pair<int, int> b){
+    return a.second < b.second ? true : false;
 }
 
 int main(){
@@ -52,28 +20,68 @@ int main(){
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> T;
-    while(T--){
-        for(int i=0; i<2001; i++){
-            v[i].clear();
-            S[i]=2e9;
-            G[i]=2e9;
-            H[i]=2e9;
-        }
-        dist.clear();
-        ans.clear();
-        cin >> n >> m >> t;
-        cin >> s >> g >> h;
-        for(int i=0; i<m; i++){
-            cin >> a >> b >> d;
-            v[a].push_back({b, d});
-            v[b].push_back({a, d});
-        }
-        for(int i=0; i<t; i++){
-            int temp;
-            cin >> temp;
-            dist.push_back(temp);
-        }
-        solve();
+    for(int i=0; i<5; i++){
+        char c;
+        int n;
+        cin >> c >> n;
+        card[i]={c, n};
     }
+    sort(card, card+5, comp);
+
+    bool check=true;
+    char color=card[0].first;
+    for(int i=1; i<5; i++)
+        if(color!=card[i].first) check=false; //색 확인
+
+    if(check==true){ //5장 같은색
+        bool con=true;
+        int diff=card[0].second-card[1].second;
+        for(int i=1; i<4; i++)
+            if(card[i].second-card[i+1].second!=diff) con=false; //연속 확인
+        if(con==true) score.push_back(900+card[4].second); //1
+        else score.push_back(600+card[4].second); //4
+    }
+    else{
+        bool con=true;
+        int diff=card[0].second-card[1].second;
+        for(int i=1; i<4; i++)
+            if(card[i].second-card[i+1].second!=diff) con=false; //연속 확인
+        if(con==true) score.push_back(500+card[4].second); //5
+
+        vector<pair<int, int>> v; //카드숫자, 같은 숫자 수
+        for(int i=0; i<5; i++){
+            bool same=false;
+            for(int j=0; j<v.size(); j++){
+                if(card[i].second==v[j].first){
+                    v[j].second++;
+                    same=true;
+                }
+            }
+            if(same==false) v.push_back({card[i].second, 1});
+        }
+        sort(v.begin(), v.end(), comp2);
+
+        if(v.size()==2){ //4,1 or 3,2
+            if(v[1].second==4)
+                score.push_back(800+v[1].first); //2
+            else
+                score.push_back(700+v[1].first*10+v[0].first); //3
+        }
+        else if(v.size()==3){ //3,1,1 or 2,2,1
+            if(v[2].second==3)
+                score.push_back(400+v[2].first); //6
+            else
+                score.push_back(max(v[1].first, v[2].first)*10+min(v[1].first, v[2].first)+300); //7
+        }
+        else if(v.size()==4){ //2,1,1,1
+            score.push_back(v[3].first+200); //8
+        }
+        else{ //1,1,1,1,1
+            score.push_back(100+card[4].second);
+        }
+    }
+    int MAX=0;
+    for(int i=0; i<score.size(); i++)
+        MAX=max(MAX, score[i]);
+    cout << MAX;
 }
