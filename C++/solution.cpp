81a49a61367479d003b6f78390858bc3d166f9b0
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-05 10:35:21 +0900

    영역 구하기(2583)

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,34 +1,77 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-int n, k;
-int gold[1001];
-int silver[1001];
-int bronze[1001];
+int m, n, k, arr[100][100];
+bool visit[100][100];
+int dx[]={1, 0, -1, 0};
+int dy[]={0, 1, 0, -1};
+vector<int> v;
 int answer;
+
+int bfs(int x, int y){
+    queue<pair<int, int>> q;
+    q.push({x, y});
+    visit[x][y]=true;
+    int area=1;
+
+    while(!q.empty()){
+        int cx=q.front().first;
+        int cy=q.front().second;
+        q.pop();
+
+        for(int i=0; i<4; i++){
+            int nx=cx+dx[i];
+            int ny=cy+dy[i];
+
+            if(nx>=0 && ny>=0 && nx<m && ny<n){
+                if(arr[nx][ny]==0 && visit[nx][ny]==false){
+                    visit[nx][ny]=true;
+                    q.push({nx, ny});
+                    area++;
+                }
+            }
+        }
+    }
+
+    return area;
+}
 
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n >> k;
-    for(int i=1; i<=n; i++){
-       int a, b, c, d;
-       cin >> a >> b >> c >> d;
-       gold[a]=b; silver[a]=c; bronze[a]=d;
+    cin >> m >> n >> k;
+    while(k--){
+        int ldx, ldy, rux, ruy;
+        cin >> ldx >> ldy >> rux >> ruy;
+        int lux=ldx, luy=ruy, rdx=rux, rdy=ldy; //좌상단, 우하단 좌표
+
+        //좌표를 인덱스로 표현
+        int left_idx_m=m-luy;
+        int left_idx_n=lux;
+        int right_idx_m=m-rdy-1;
+        int right_idx_n=rdx-1;
+
+        //인덱스를 기반으로 사각형 표현
+        for(int i=left_idx_m; i<=right_idx_m; i++)
+            for(int j=left_idx_n; j<=right_idx_n; j++)
+                arr[i][j]=1;
     }
-    for(int i=1; i<=n; i++){
-        if(i==k) continue;
-        if(gold[k]<gold[i]) answer++;
-        else if(gold[k]==gold[i]){
-            if(silver[k]<silver[i]) answer++;
-            else if(silver[k]==silver[i]){
-                if(bronze[k]<bronze[i]) answer++;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(arr[i][j]==0 && visit[i][j]==false){
+                v.push_back(bfs(i, j));
+                answer++;
             }
         }
     }
-    cout << answer+1;
+    sort(v.begin(), v.end());
+    cout << answer << "\n";
+    for(int i=0; i<v.size(); i++)
+        cout << v[i] << " ";
 }
