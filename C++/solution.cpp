be7fa2eae2af11aebc2787cbfabf3af587ed4e12
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-11 17:06:15 +0900

    멀티탭 스케줄링(1700)

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -2,44 +2,52 @@
 
 using namespace std;
 
-int r, c, MAX;
-char arr[20][20];
-bool visit[20][20];
-int dx[]={1, 0, -1, 0};
-int dy[]={0, 1, 0, -1};
-
-void dfs(int x, int y, int cnt, bool alpha[20]){
-    for(int i=0; i<4; i++){
-        int nx=x+dx[i];
-        int ny=y+dy[i];
-        if(nx>=0 && ny>=0 && nx<r && ny<c){
-            if(visit[nx][ny]==false && alpha[arr[nx][ny]-'A']==false){
-                visit[nx][ny]=true;
-                alpha[arr[nx][ny]-'A']=true;
-                dfs(nx, ny, cnt+1, alpha);
-                visit[nx][ny]=false;
-                alpha[arr[nx][ny]-'A']=false;
-            }
-        }
-    }
-    MAX=max(MAX, cnt);
-}
+int n, k, answer;
+int order[101], mul[101];
 
 int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> r >> c;
-    for(int i=0; i<r; i++){
-        string s;
-        cin >> s;
-        for(int j=0; j<c; j++)
-            arr[i][j]=s[j];
+    cin >> n >> k;
+    for(int i=1; i<=k; i++)
+        cin >> order[i];
+
+    for(int i=1; i<=k; i++){
+        //꽂혀있는 것을 사용
+        bool flag=false;
+        for(int j=1; j<=n; j++)
+            if(mul[j]==order[i]) flag=true;
+        if(flag==true) continue;
+
+        //빈칸에 꽂기
+        for(int j=1; j<=n; j++){
+            if(mul[j]==0){
+                mul[j]=order[i];
+                flag=true;
+                break;
+            }
+        }
+        if(flag==true) continue;
+
+        //뽑고 꽂기
+        //꽂혀있는 것 중 가장 나중에 사용되는 것 뽑기
+        int last=-1;
+        int idx=-1;
+        for(int j=1; j<=n; j++){
+            int temp=0;
+            for(int l=i+1; l<=k; l++){
+                if(order[l]==mul[j]) break;
+                temp++;
+            }
+            if(temp>last){
+                last=temp;
+                idx=j;
+            }
+        }
+        mul[idx]=order[i];
+        answer++;
     }
-    bool alpha[26]={0};
-    visit[0][0]=true;
-    alpha[arr[0][0]-'A']=true;
-    dfs(0, 0, 1, alpha);
-    cout << MAX;
+    cout << answer;
 }
