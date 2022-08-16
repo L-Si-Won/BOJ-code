4becd13343e2c55080838cdff42323d1a6ccc928
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-16 13:47:25 +0900

    로봇 프로젝트(3649)

--- C++/test3.cpp
+++ C++/test3.cpp
@@ -1,198 +1,48 @@
 #include <iostream>
 #include <vector>
-#define max(a, b) a > b ? a : b
+#include <algorithm>
 
 using namespace std;
-typedef long long ll;
 
-int n;
-ll ans;
+vector<int> v;
+int x;
 
-// board에서 최대 값 구하는 함수
-ll getMax(vector<vector<ll>> board)
-{
-    ll res = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            res = max(res, board[i][j]);
-        }
-    }
-    return res;
-}
-
-// 오른쪽 이동
-vector<vector<ll>> right(vector<vector<ll>> board)
-{
-    vector<vector<bool>> check(n, vector<bool>(n, false));
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = n - 2; j >= 0; j--)
-        {
-            if (board[i][j] == 0)
-                continue;
-            for (int k = j + 1; k < n; k++)
-            {
-                if (board[i][k] == board[i][k - 1] && !check[i][k])
-                {
-                    board[i][k] *= 2;
-                    board[i][k - 1] = 0;
-                    check[i][k] = true;
-                    break;
-                }
-                else if (board[i][k] == 0)
-                {
-                    board[i][k] = board[i][k - 1];
-                    board[i][k - 1] = 0;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-    }
-    return board;
-}
-
-// 왼쪽 이동
-vector<vector<ll>> left(vector<vector<ll>> board)
-{
-    vector<vector<bool>> check(n, vector<bool>(n, false));
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j < n; j++)
-        {
-            if (board[i][j] == 0)
-                continue;
-            for (int k = j - 1; k >= 0; k--)
-            {
-                if (board[i][k] == board[i][k + 1] && !check[i][k])
-                {
-                    board[i][k] *= 2;
-                    board[i][k + 1] = 0;
-                    check[i][k] = true;
-                    break;
-                }
-                else if (board[i][k] == 0)
-                {
-                    board[i][k] = board[i][k + 1];
-                    board[i][k + 1] = 0;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-    }
-    return board;
-}
-
-// 아래 이동
-vector<vector<ll>> down(vector<vector<ll>> board)
-{
-    vector<vector<bool>> check(n, vector<bool>(n, false));
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = n - 2; j >= 0; j--)
-        {
-            if (board[j][i] == 0)
-                continue;
-            for (int k = j + 1; k < n; k++)
-            {
-                if (board[k][i] == board[k - 1][i] && !check[k][i])
-                {
-                    board[k][i] *= 2;
-                    board[k - 1][i] = 0;
-                    check[k][i] = true;
-                    break;
-                }
-                else if (board[k][i] == 0)
-                {
-                    board[k][i] = board[k - 1][i];
-                    board[k - 1][i] = 0;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-    }
-    return board;
-}
-
-// 위로 이동
-vector<vector<ll>> up(vector<vector<ll>> board)
-{
-    vector<vector<bool>> check(n, vector<bool>(n, false));
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j < n; j++)
-        {
-            if (board[j][i] == 0)
-                continue;
-            for (int k = j - 1; k >= 0; k--)
-            {
-                if (board[k][i] == board[k + 1][i] && !check[k][i])
-                {
-                    board[k][i] *= 2;
-                    board[k + 1][i] = 0;
-                    check[k][i] = true;
-                    break;
-                }
-                else if (board[k][i] == 0)
-                {
-                    board[k][i] = board[k + 1][i];
-                    board[k + 1][i] = 0;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-    }
-    return board;
-}
-
-void dfs(int L, vector<vector<ll>> board)
-{
-    ans = max(ans, getMax(board));
-    if (L == 5)
-        return;
-    dfs(L + 1, right(board));
-    dfs(L + 1, left(board));
-    dfs(L + 1, up(board));
-    dfs(L + 1, down(board));
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
+int main() {
     cin.tie(NULL);
     cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
-    // 입력
-    cin >> n;
-    vector<vector<ll>> board(n, vector<ll>(n));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> board[i][j];
+    while (cin >> x){
+        x*=10000000; //나노변환
+        int n;
+        cin >> n;
+        /*
+        if(n<2){
+            cout << "danger\n";
+            continue;
         }
+        */
+        v.clear();
+        for(int i=0;i<n;i++){
+            int temp;
+            cin>> temp;
+            v.push_back(temp);   
+        }
+        sort(v.begin(),v.end());
+
+        int l=0,r=n-1,sum;
+        bool flag=false;
+        while(l<r){ //이분탐색
+            sum = v[l]+v[r];
+            if(sum==x){
+                flag = true;
+                cout << "yes " << v[l] << " " << v[r] << '\n';
+                break;
+            }
+            else if(sum < x)  l++;
+            else r--;
+        }
+        if(flag==false) cout << "danger\n";
     }
-
-    // dfs 실행
-    dfs(0, board);
-    cout << ans << '\n';
-
     return 0;
 }
