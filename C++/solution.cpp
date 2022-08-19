d0a2a6d7820a7f8da08ed3be4c1a5cf0d8d8b72a
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-19 10:05:21 +0900

    수리공 항승(1449)

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <string>
-#include <map>
 
 using namespace std;
 
-int n, m;
-map<string, int> man;
-vector<string> answer;
+int n, l, answer;
+bool holl[1001];
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n >> m;
+    cin >> n >> l;
     for(int i=0; i<n; i++){
-        string s;
-        cin >> s;
-        man.insert({s, 1});
+        int a;
+        cin >> a;
+        holl[a]=true;
     }
-    for(int i=0; i<m; i++){
-        string s;
-        cin >> s;
-        if(man.find(s)==man.end()) man.insert({s, 1});
-        else man[s]++;
-    }
-    for(auto i=man.begin(); i!=man.end(); i++)
-        if(i->second==2) answer.push_back(i->first);
     
-    sort(answer.begin(), answer.end());
-    cout << answer.size() << "\n";
-    for(int i=0; i<answer.size(); i++)
-        cout << answer[i] << "\n";
+    for(int i=0; i<=1000; i++){
+        if(holl[i]==true){
+            for(int j=0; j<l; j++){
+                if(i+j<=1000 && holl[i+j]==true) holl[i+j]=false;
+            }
+            answer++;
+        }
+    }
+
+    cout << answer;
 }
