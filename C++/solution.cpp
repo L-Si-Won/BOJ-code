85ce6ca08d73304c0aa32003707dba51c1e62e74
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-21 13:17:02 +0900

    수들의 합2(2003)

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -2,31 +2,35 @@
 
 using namespace std;
 
-int n;
-string s;
-
-void solve(string s, int cnt){
-    for(int i=1; i<=s.size()/2; i++)
-        if(s.substr(s.size()-i, i)==s.substr(s.size()-i*2, i))
-            return;
-    
-    if(cnt==n){
-        cout << s;
-        exit(0);
-    }
-
-    for(int i=0; i<n; i++){
-        solve(s+"1", cnt+1);
-        solve(s+"2", cnt+1);
-        solve(s+"3", cnt+1);
-    }
-}
+int n, m, answer;
+int arr[10001];
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    solve("", 0);
+    cin >> n >> m;
+    for(int i=1; i<=n; i++)
+        cin >> arr[i];
+    
+    int start=0, end=0, sum=0;
+    while(start<=end && end<=n){
+        if(sum==m) answer++;
+
+        if(sum<=m){
+            end++;
+            if(end<=n) sum+=arr[end];
+        }
+        else if(sum>m){
+            sum-=arr[start];
+            start++;
+            if(start>end){
+                end++;
+                if(end<=n) sum+=arr[end];
+            }
+        }
+    }
+
+    cout << answer;
 }
