206e95170f47e4bc815e3ef6347f35fc93ebd9f9
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-23 12:38:54 +0900

    피보나치 수2(2748)

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -1,35 +1,14 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-long long n, m, answer;
-vector<long long> tree;
+long long n, dp[91];
 
-long long calculate(long long l){
-    long long sum=0;
-    for(int i=0; i<tree.size(); i++)
-        if(tree[i]>=l) sum+=tree[i]-l;
-    return sum;
-}
-
-void solve(){
-    long long left=0, right=tree[tree.size()-1];
-    long long mid, sum;
-    while(left<=right){
-        mid=(left+right)/2;
-        sum=calculate(mid);
-
-        if(sum>=m){
-            answer=mid;
-            left=mid+1;
-        }
-        else if(sum<m)
-            right=mid-1;
-    }
-    
-    cout << answer;
+void fibo(){
+    dp[1]=1;
+    for(int i=2; i<=n; i++)
+        dp[i]=dp[i-1]+dp[i-2];
 }
 
 int main() {
@@ -37,14 +16,7 @@ int main() {
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n >> m;
-    for(int i=0; i<n; i++){
-        long long a;
-        cin >> a;
-        tree.push_back(a);
-    }
-
-    sort(tree.begin(), tree.end());
-
-    solve();
+    cin >> n;
+    fibo();
+    cout << dp[n];
 }
