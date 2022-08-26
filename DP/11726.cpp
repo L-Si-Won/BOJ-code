504d4e76580120a4f0bd98f61064896c37d1dfcc
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-26 19:34:58 +0900

    2×n 타일링(11726)

---
+++ DP/11726.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int d[1001];
+
+int dp(int i)
+{
+    if(i==1) return 1;
+    if(i==2) return 2;
+    if(d[i]!=0) return d[i];
+    return d[i]=(dp(i-1)+dp(i-2))%10007;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << dp(n);
+}
