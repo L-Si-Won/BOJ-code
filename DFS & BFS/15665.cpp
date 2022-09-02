1e5acd5cfa64539ebbe3725044a34c37af90fbb7
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-09-02 15:36:11 +0900

    N과 M (11)(15665)

---
+++ DFS & BFS/15665.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+int temp[9];
+bool visit[9];
+
+void dfs(int cnt){
+    if(cnt==m){
+        for(int i=0; i<m; i++)
+            cout << temp[i] << " ";
+        cout << "\n";
+        return ;
+    }
+    int check=0;
+    for(int i=1; i<n+1; i++){
+        if(arr[i]!=check){
+            temp[cnt]=arr[i];
+            check=temp[cnt];
+            dfs(cnt+1);
+        }
+    }
+}
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n >> m;
+    for(int i=1; i<n+1; i++)
+        cin >> arr[i];
+    sort(arr+1, arr+n+1);
+    dfs(0);
+}
