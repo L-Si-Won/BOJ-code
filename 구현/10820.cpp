94e37518a85b4bedaef55824f418865c4867fe9f
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-09-02 15:08:09 +0900

    문자열 분석(10820)

---
+++ 구현/10820.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    while(getline(cin, s))
+    {
+        int a=0, b=0, c=0, d=0;
+        for(int j=0; j<s.size(); j++)
+        {
+            if(s[j]>='a' && s[j]<='z')
+                a++;
+            if(s[j]>='A' && s[j]<='Z')
+                b++;
+            if(s[j]==' ')
+                d++;
+            if(s[j]>='0' && s[j]<='9')
+                c++;
+        }
+        cout << a << " " << b << " " << c << " " << d << '\n';
+    }
+}
