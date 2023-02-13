4895e1f808581c62dbb85eba1ef099143ac05802
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2023-02-13 18:03:49 +0900

    골드바흐의 추측(6588)

--- 구현/6588.cpp
+++ 구현/6588.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <cmath>
 
@@ -31,6 +32,47 @@ int main(){
             if(prime_check(i) && prime_check(n-i)){
                 flag=true;
                 a=i;
+                break;
+            }
+        }
+
+        if(flag)
+            printf("%d = %d + %d\n", n, a, n-a);
+        else
+            cout << "Goldbach's conjecture is wrong.\n";
+    }
+}
+***********************************************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+bool prime[1000001];
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    for(int i=2; i*i<=1000000; i++){
+        if (prime[i] == 0)
+            for(int j=i+i; j<=1000000; j+=i)
+                prime[j] = 1;
+    }
+
+    //추측 검증
+    while(1){
+        cin >> n;
+        if(n==0) break;
+
+        bool flag=false;
+        int a;
+        for(int i=3; i<=n; i+=2){
+            if(prime[i] == 0 && prime[n-i] == 0){
+                a = i;
+                flag = true;
                 break;
             }
         }
