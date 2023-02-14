f47efa7e5683341b775f8fe59c58840e5a895189
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2023-02-14 14:00:31 +0900

    레이저 통신(6087)

--- DFS & BFS/6087.cpp
+++ DFS & BFS/6087.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <queue>
 
@@ -60,6 +61,74 @@ int main(){
                     c2={i, j};
             }
             cnt[i][j]=2e9;
+        }
+    }
+    solve();
+}
+****************************************************************************************************************/
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int h, w; //행, 열
+char arr[100][100];
+pair<int, int> c1={-1, -1};
+pair<int, int> c2={-1, -1};
+int dx[]={1, 0, -1, 0};
+int dy[]={0, 1, 0, -1};
+int mir_cnt[100][100];
+
+void solve(){
+    queue<pair<pair<int, int>, int>> q; //좌표, 거울 수
+    q.push({c1, 0});
+    mir_cnt[c1.first][c1.second]=0;
+
+    while(!q.empty()){
+        int x=q.front().first.first;
+        int y=q.front().first.second;
+        int mir=q.front().second;
+        q.pop();
+        
+        for(int i=0; i<4; i++){ //순서 : 하(0), 우(1), 상(2), 좌(3)
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+
+            while (nx>=0 && nx<h && ny>=0 && ny<w){
+                if (arr[nx][ny] == '*')
+                    break;
+                else{
+                    if (mir_cnt[nx][ny] == 2e9){
+                        mir_cnt[nx][ny] = mir;
+                        q.push({{nx, ny}, mir+1});
+                    }
+                }
+                nx += dx[i];
+                ny += dy[i];
+            }
+        }
+    }
+    cout << mir_cnt[c2.first][c2.second] <<"\n";
+}
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> w >> h;
+    for(int i=0; i<h; i++){
+        string s;
+        cin >> s;
+        for(int j=0; j<w; j++){
+            arr[i][j]=s[j];
+            if(arr[i][j]=='C'){
+                if(c1.first==-1)
+                    c1={i, j};
+                else
+                    c2={i, j};
+            }
+            mir_cnt[i][j]=2e9;
         }
     }
     solve();
