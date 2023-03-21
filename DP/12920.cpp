d1e08144851cec719087dad7adfcadc48fef5f4b
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2023-03-21 21:27:08 +0900

    평범한 배낭 2(12920)

--- DP/12920.cpp
+++ DP/12920.cpp
@@ -56,3 +56,64 @@ int main(){
     //output
     cout << d[thing.size() - 1][m];
 }
+
+/*****************************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+struct something{
+    int v;
+    int c;
+};
+
+int n, m;
+vector<something> thing;
+int d[2][10001];
+
+void dp(){
+    for(int i = thing[0].v; i <= m; i++)
+        d[0][i] = thing[0].c;
+
+    for(int i = 1; i < thing.size(); i++){
+        for(int j = 0; j < thing[i].v; j++)
+            d[i % 2][j] = d[(i % 2) == 0 ? 1 : 0][j];
+
+        for(int j = thing[i].v; j <= m; j++)
+            d[i % 2][j] = max(d[(i % 2) == 0 ? 1 : 0][j], d[(i % 2) == 0 ? 1 : 0][j - thing[i].v] + thing[i].c);
+    }
+}
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    //input
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        int v, c, k;
+        cin >> v >> c >> k;
+
+        //bind thing
+        int two = 1;
+        while(k > 0){
+            if (two > k)
+                two = k;
+
+            thing.push_back({v * two, c * two});
+
+            k -= two;
+            two *= 2;
+        }
+    }
+
+    //dp
+    dp();
+    
+    //output
+    cout << d[(thing.size() % 2) == 0 ? 1 : 0][m];
+}
