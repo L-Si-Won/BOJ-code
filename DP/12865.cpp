fe27b9e7e2502bc0321537125199fce57f2a0ecd
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2023-03-21 22:37:24 +0900

    평범한 배낭(12865)

--- DP/12865.cpp
+++ DP/12865.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <map>
 
 using namespace std;
 
@@ -32,7 +31,6 @@ int main(){
 /************************************************************************/
 
 #include <iostream>
-#include <map>
 
 using namespace std;
 
