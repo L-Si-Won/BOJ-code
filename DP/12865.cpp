665281084ad63ad23b8b985b82c52d44e8113845
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2023-03-21 22:30:16 +0900

    평범한 배낭(12865)

--- DP/12865.cpp
+++ DP/12865.cpp
@@ -28,3 +28,38 @@ int main(){
         cin >> w[i] >> v[i];
     solve();
 }
+
+/************************************************************************/
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int n, k;
+int w[101];
+int v[101];
+int dp[2][100001];
+
+void solve(){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=k; j++){
+            if(j-w[i]>=0)
+				dp[i % 2][j]=max(dp[i % 2 == 0 ? 1 : 0][j], dp[i % 2 == 0 ? 1 : 0][j-w[i]]+v[i]);
+            else
+				dp[i % 2][j]=dp[i % 2 == 0 ? 1 : 0][j];
+        }
+    }
+    cout << dp[n % 2 == 0 ? 0 : 1][k];
+}
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n >> k;
+    for(int i=1; i<=n; i++)
+        cin >> w[i] >> v[i];
+    solve();
+}
