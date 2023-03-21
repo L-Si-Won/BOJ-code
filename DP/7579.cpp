2505c31c11286ce1539b4bd54e345d8fd3b865f2
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2023-03-21 22:50:13 +0900

    앱(7579)

--- DP/7579.cpp
+++ DP/7579.cpp
@@ -35,3 +35,45 @@ int main(){
     }
     solve();
 }
+
+/*******************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int n, m, sum;
+int mem[101], cost[101];
+int dp[2][10001];
+
+void solve(){
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<=sum; j++){
+            if(j>=cost[i])
+				dp[i % 2][j]=mem[i]+dp[i % 2 == 0 ? 1 : 0][j-cost[i]];
+
+            dp[i % 2][j]=max(dp[i % 2][j], dp[i % 2 == 0 ? 1 : 0][j]);
+        }
+    }
+    for(int i=1; i<=sum; i++){
+        if(dp[n % 2 == 0 ? 0 : 1][i]>=m){
+            cout << i;
+            break;
+        }
+    }
+}
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n >> m;
+    for(int i=1; i<=n; i++)
+        cin >> mem[i];
+    for(int i=1; i<=n; i++){
+        cin >> cost[i];
+        sum+=cost[i];
+    }
+    solve();
+}
