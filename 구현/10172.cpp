b6900b913210374157beac47ae5792c47191973d
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2023-04-21 13:42:42 +0900

    개(10172)

---
+++ 구현/10172.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cout << "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n";
+}
