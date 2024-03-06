9d62a412681a94661c851ca67744ccc5e053f555
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2024-03-06 18:15:32 +0900

    아스키 코드(11654)

---
+++ 구현/11654.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+char c;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> c;
+    cout << (int)c;
+}
