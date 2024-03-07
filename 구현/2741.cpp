867d072bd0c275b19c5ad8304928785e32e14e66
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2024-03-07 16:34:22 +0900

    N 찍기(2741)

---
+++ 구현/2741.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cout << i << "\n";
+}
