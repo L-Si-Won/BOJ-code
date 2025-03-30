79a5e66eda3d3d640913ec9c29e2c7216dc2f698
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2025-03-30 18:14:19 +0900

    경로 찾기(11403)

---
+++ Algorithm/11403.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+unsigned int arr[100][100];
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 0) {
+                arr[i][j] = 2e9;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                arr[j][k] = min(arr[j][k], arr[j][i] + arr[i][k]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[i][j] == 2e9) {
+                cout << 0 << " ";
+            } else {
+                cout << 1 << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
